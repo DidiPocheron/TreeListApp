08d5bb9247dfd25829e45ada4d379b0929c2a81d
Dimitri <dimitri@sadighgroup.com>
2022-02-25 20:37:54 +0100

    Init project

---
+++ TreeListApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TreeListApp
+//
+//  Created by Didi on 25/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TreeListApp/TreeListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TreeListAppApp.swift
+//  TreeListApp
+//
+//  Created by Didi on 25/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct TreeListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
